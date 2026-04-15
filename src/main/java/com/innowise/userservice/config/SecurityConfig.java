e10c60700670b196e0bb202d7e01fa67b9e66ed0
Darya <darya.m.2006@mail.ru>
2026-04-15 19:00:48 +0300

    Enable Kubernetes-friendly health probes

--- src/main/java/com/innowise/userservice/config/SecurityConfig.java
+++ src/main/java/com/innowise/userservice/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(request -> request
                     .requestMatchers(
+                            "/actuator/**",
                             "/api/v1/auth/**",
                             "/v3/api-docs/**",
                             "/swagger-ui/**",
